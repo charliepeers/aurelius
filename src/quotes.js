62da00a94f6f69ab2ee4ad00ccc65b223471cc98
CharliePeers <63914466+CharliePeers@users.noreply.github.com>
2026-02-05 22:43:32 -0500

    initial Stoic quote dataset

---
+++ src/quotes.js
@@ -0,0 +1,20 @@
+export const quotes = [{
+    author: "Marcus Aurelius",
+    text: "You have power over your mind - not outside events. Realize this, and you will find strength."
+  },
+  {
+    author: "Seneca",
+    text: "We suffer more often in imagination than in reality."
+  },
+  {
+    author: "Epictetus",
+    text: "No man is free who is not master of himself."
+  },
+  {
+    author: "Marcus Aurelius",
+    text: "The happiness of your life depends upon the quality of your thoughts."
+  },
+  {
+    author: "Seneca",
+    text: "It is not that we have a short time to live, but that we waste a lot of it."
+  }];
